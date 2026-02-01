614e810c49246e70f9ba33758b133a969c87ee60
Trung Tin Truong <trungtin.230599@hotmail.com>
2026-02-01 18:35:51 +0000

    Fix Voice migrations

--- Infrastructure/Persistence/Migrations/20260201165233_AddVoiceToUserSettings.cs
+++ Infrastructure/Persistence/Migrations/20260201165233_AddVoiceToUserSettings.cs
@@ -10,13 +10,21 @@ namespace Infrastructure.Persistence.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.AddColumn<string>(
+                name: "Voice",
+                table: "UserSettings",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.DropColumn(
+                name: "Voice",
+                table: "UserSettings");
         }
     }
 }
---
+++ Infrastructure/Persistence/Migrations/20260201183426_AddVoiceColumnHotfix.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddVoiceColumnHotfix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "IF COL_LENGTH('UserSettings', 'Voice') IS NULL " +
+                "BEGIN " +
+                "ALTER TABLE [UserSettings] ADD [Voice] nvarchar(200) NOT NULL CONSTRAINT [DF_UserSettings_Voice] DEFAULT(''); " +
+                "END");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "IF COL_LENGTH('UserSettings', 'Voice') IS NOT NULL " +
+                "BEGIN " +
+                "ALTER TABLE [UserSettings] DROP CONSTRAINT [DF_UserSettings_Voice]; " +
+                "ALTER TABLE [UserSettings] DROP COLUMN [Voice]; " +
+                "END");
+        }
+    }
+}
